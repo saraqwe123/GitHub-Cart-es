bb5499742db1f028cce11e397fe28e9bd2542416
Sara Guaiume <130059301+saraqwe123@users.noreply.github.com>
2024-06-26 11:41:27 -0300

    26/06
    baixando tailwind e estilizando a página, fiz vários testes e ainda estou aprendendo a mexer

---
+++ ATIVIDADE-CARTAO-main/cartao/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        roxo: "#8A2BE2", // Definindo uma nova cor roxo mais escura
+      },
+    },
+  },
+  plugins: [],
+};
